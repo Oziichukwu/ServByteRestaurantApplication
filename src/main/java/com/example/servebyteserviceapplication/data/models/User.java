13492a5261e880d018f512efa6b2b270f7d786f3
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-01 21:38:05 +0100

    user repository and model updated

--- src/main/java/com/example/servebyteserviceapplication/data/models/User.java
+++ src/main/java/com/example/servebyteserviceapplication/data/models/User.java
@@ -1,2 +1,31 @@
-package com.example.servebyteserviceapplication.data.models;public class User {
+package com.example.servebyteserviceapplication.data.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+
+
+    @Column(length = 500)
+    private String address;
 }
--- src/main/java/com/example/servebyteserviceapplication/data/repositories/UserRepository.java
+++ src/main/java/com/example/servebyteserviceapplication/data/repositories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.servebyteserviceapplication.data.repositories;public class UserRepository {
+package com.example.servebyteserviceapplication.data.repositories;
+
+import com.example.servebyteserviceapplication.data.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
