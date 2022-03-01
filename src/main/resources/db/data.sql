83cc5fde85369863b5a9d9a757f490aae824d87e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-01 17:02:11 +0100

    resources file

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO user (id, first_name,last_name,email,phone_number) VALUES (1, 'demola', 'ajayi','demola@gmail.com','08156565689');
+INSERT INTO user (id,first_name,last_name,email,phone_number ) VALUES (2, 'bisi', 'bolaji','bisi@gmail.com','34344343434');
+INSERT INTO user (id,first_name,last_name,email,phone_number ) VALUES (3, 'taiwo', 'solanke', 'taiwo@gmail.com','09156543532');
