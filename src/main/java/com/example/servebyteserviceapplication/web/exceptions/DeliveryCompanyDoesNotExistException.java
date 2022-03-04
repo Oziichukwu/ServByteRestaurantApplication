3ea19858215ffc2dd6006ada3ed641085a655cb8
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-04 03:00:56 +0100

    logistic exception

--- src/main/java/com/example/servebyteserviceapplication/web/exceptions/DeliveryCompanyDoesNotExistException.java
+++ src/main/java/com/example/servebyteserviceapplication/web/exceptions/DeliveryCompanyDoesNotExistException.java
@@ -1,2 +1,22 @@
-package com.example.servebyteserviceapplication.web.exceptions;public class DeliveryCompanyDoesNotExistException {
+package com.example.servebyteserviceapplication.web.exceptions;
+
+public class DeliveryCompanyDoesNotExistException extends ServByteServiceException {
+    public DeliveryCompanyDoesNotExistException() {
+    }
+
+    public DeliveryCompanyDoesNotExistException(String message) {
+        super(message);
+    }
+
+    public DeliveryCompanyDoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeliveryCompanyDoesNotExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public DeliveryCompanyDoesNotExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/example/servebyteserviceapplication/web/exceptions/LogisticServiceDoesNotExist.java
+++ src/main/java/com/example/servebyteserviceapplication/web/exceptions/LogisticServiceDoesNotExist.java
@@ -1,2 +1,22 @@
-package com.example.servebyteserviceapplication.web.exceptions;public class LogisticServiceDoesNotExist {
+package com.example.servebyteserviceapplication.web.exceptions;
+
+public class LogisticServiceDoesNotExist extends ServByteServiceException {
+    public LogisticServiceDoesNotExist() {
+    }
+
+    public LogisticServiceDoesNotExist(String message) {
+        super(message);
+    }
+
+    public LogisticServiceDoesNotExist(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LogisticServiceDoesNotExist(Throwable cause) {
+        super(cause);
+    }
+
+    public LogisticServiceDoesNotExist(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
