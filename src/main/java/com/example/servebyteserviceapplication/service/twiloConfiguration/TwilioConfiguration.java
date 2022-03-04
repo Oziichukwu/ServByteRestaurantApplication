0c6b982413121e6c0f8891e494d52ab785308991
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-04 15:37:48 +0100

    exception

--- src/main/java/com/example/servebyteserviceapplication/service/twiloConfiguration/TwilioConfiguration.java
+++ src/main/java/com/example/servebyteserviceapplication/service/twiloConfiguration/TwilioConfiguration.java
@@ -1,2 +1,19 @@
-package com.example.servebyteserviceapplication.service.twiloConfiguration;public class TwilioConfiguration {
+package com.example.servebyteserviceapplication.service.twiloConfiguration;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("twilio")
+@Data
+public class TwilioConfiguration {
+
+    private String accountSid;
+    private String authToken;
+    private String trialNumber;
+
+    public TwilioConfiguration() {
+    }
 }
--- src/main/java/com/example/servebyteserviceapplication/web/exceptions/PhoneNumberDoesNotExistException.java
+++ src/main/java/com/example/servebyteserviceapplication/web/exceptions/PhoneNumberDoesNotExistException.java
@@ -1,2 +1,22 @@
-package com.example.servebyteserviceapplication.web.exceptions;public class PhoneNumberDoesNotExistException {
+package com.example.servebyteserviceapplication.web.exceptions;
+
+public class PhoneNumberDoesNotExistException extends ServByteServiceException {
+    public PhoneNumberDoesNotExistException() {
+    }
+
+    public PhoneNumberDoesNotExistException(String message) {
+        super(message);
+    }
+
+    public PhoneNumberDoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PhoneNumberDoesNotExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public PhoneNumberDoesNotExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
